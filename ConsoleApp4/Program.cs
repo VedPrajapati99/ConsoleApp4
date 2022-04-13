c0d0b5f8a1ee86ece4500e32884a10877dc76ad0
vedpr <vedpr@VED-DESKTOP>
2022-04-13 18:13:01 +0100

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿//See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+Double test = 45.0 / 2;
+Console.WriteLine(test);
+
+double remain = 5 % 20;
+Console.WriteLine(remain);
+
+int result = (2 + 3) * 4;
+Console.WriteLine("The Answer is {0}", result);
+
+
+int x = 100;
+x = x + 6;
+Console.WriteLine("The Value of X is {0}", x);
+
+int y = 5;
+y++;
+Console.WriteLine("The Value of y is {0}", y);
+
+int b = 4;
+int c = ++b;
+Console.WriteLine("the value of b is {0} and the value of c is {1}", b, c);
